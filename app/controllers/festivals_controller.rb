64723ac13dc9ac8d6cbda99bb9e49e617ef3ecf4
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-05-14 19:15:25 -0700

    removes location model; adds location data to festivals model; edits respective controllers and forms

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -15,7 +15,7 @@ class FestivalsController < ApplicationController
 	end
 
 	def create
-		@festival = Festival.new(params.require(:festival).permit(:name, :type, :neighborhood, :address, :details))
+		@festival = Festival.new(params.require(:festival).permit(:name, :type, :neighborhood, :details, :street, :city, :state, :zip, :latitude, :longitude, :coordinates))
  		fest_date = params[:festival]
 		@festival.fest_date = Date.new fest_date["fest_date(1i)"].to_i, fest_date["fest_date(2i)"].to_i, fest_date["fest_date(3i)"].to_i
 		start_time = params[:festival]
@@ -42,7 +42,7 @@ class FestivalsController < ApplicationController
 		fest_date = params[:festival]
 		start_time = params[:festival]
 		end_time = params[:festival]
-		if @festival.update_attributes(params.require(:festival).permit(:name, :type, :neighborhood, :address, :details))
+		if @festival.update_attributes(params.require(:festival).permit(:name, :type, :neighborhood, :details, :street, :city, :state, :zip, :latitude, :longitude, :coordinates))
 			@festival.fest_date = Date.new fest_date["fest_date(1i)"].to_i, fest_date["fest_date(2i)"].to_i, fest_date["fest_date(3i)"].to_i
 			@festival.start_time = Time.new start_time["start_time(1i)"].to_i, start_time["start_time(2i)"].to_i, start_time["start_time(3i)"].to_i, start_time["start_time(4i)"].to_i, start_time["start_time(5i)"].to_i
 			@festival.end_time = Time.new end_time["end_time(1i)"].to_i, end_time["end_time(2i)"].to_i, end_time["end_time(3i)"].to_i, end_time["end_time(4i)"].to_i, end_time["end_time(5i)"].to_i
--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,5 +1,7 @@
 class Festival
+
   include Mongoid::Document
+  include Geocoder::Model::Mongoid
 
   field :name, type: String
   field :type, type: String
@@ -8,10 +10,20 @@ class Festival
   field :end_time, type: Time
   field :neighborhood, type: String
   field :details, type: String
-  field :address, type: String
-  field :latitude, type: String
-  field :longitude, type: String
+  field :street, type: String
+  field :city, type: String
+  field :state, type: String
+  field :zip, type: String
+  field :latitude, type: Float
+  field :longitude, type: Float
+  field :coordinates, type: Array
+
+  validates_presence_of :name, :type, :fest_date, :start_time, :end_time, :neighborhood, :street, :city, :state, :zip, message: "Please complete the form"
+
+  # geocoded_by :address
+  # after_validation :geocode
 
   belongs_to :neighborhood
+  belongs_to :location
 
 end
