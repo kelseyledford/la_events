37e6748f21db42bb68d15de3cae6cd7509134156
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-05-16 11:13:18 -0700

    redeploys to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Festival.destroy_all
-
-Festival.create(name: 'Ramen Festival', type: 'Food', fest_date: '7/21/2014', start_time: '11:00', end_time: '14:00', details: 'Bring your appetite!', street: '285 W Huntington Drive', city: 'Arcadia', state: 'CA', zip: '91007')
-Festival.create(name: 'Fireworks', type: 'Holiday', fest_date: '7/4/2014', start_time: '21:00', end_time: '24:00', details: 'Come celebrate Independence Day with us!', street: '100 W. Torrance Blvd.', city: 'Redondo Beach', state: 'CA', zip: '90277')
-
 Neighborhood.destroy_all
 
 Neighborhood.create(name: 'Westside')
-Neighborhood.create(name: 'South Bay')
+sb = Neighborhood.create(name: 'South Bay')
 Neighborhood.create(name: 'Long Beach')
 Neighborhood.create(name: 'Downtown')
 Neighborhood.create(name: 'Orange County')
-Neighborhood.create(name: 'The Valley')
+v = Neighborhood.create(name: 'The Valley')
 Neighborhood.create(name: 'East LA')
 Neighborhood.create(name: 'Mid-City')
 Neighborhood.create(name: 'Etc.')
+
+Festival.destroy_all
+
+v.festivals.create(name: 'Ramen Festival', type: 'Food', fest_date: '7/21/2014', start_time: '11:00', end_time: '14:00', details: 'Bring your appetite!', street: '285 W Huntington Drive', city: 'Arcadia', state: 'CA', zip: '91007')
+sb.festivals.create(name: 'Fireworks', type: 'Holiday', fest_date: '7/4/2014', start_time: '21:00', end_time: '24:00', details: 'Come celebrate Independence Day with us!', street: '100 W. Torrance Blvd.', city: 'Redondo Beach', state: 'CA', zip: '90277')
