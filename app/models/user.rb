5334796e1340bdf1c36f89be430c917529c7d85d
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-05-16 14:14:46 -0700

    adds password confirmation step for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 require "bcrypt"
 class User
   include Mongoid::Document
+  include ActiveModel::SecurePassword
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+
+  has_secure_password
 
   def password
     @password
